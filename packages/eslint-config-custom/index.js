464eb001544886ba6840c4dae4babd8dae337bf3
TMH-SE <hieuhutieu98@gmail.com>
2023-02-18 11:27:13 +0700

    Update eslint and format

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next/core-web-vitals', 'turbo', 'plugin:prettier/recommended'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
-};
+}
