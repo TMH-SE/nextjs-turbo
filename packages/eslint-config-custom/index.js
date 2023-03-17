46ff28a92b034176a38edeaf50c76402cc8ab7b2
TMH-SE <hieuhutieu98@gmail.com>
2023-03-18 00:22:39 +0700

    Config prettier

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next/core-web-vitals', 'turbo', 'prettier', 'plugin:prettier/recommended'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    '@next/next/no-html-link-for-pages': 'off',
   },
   parserOptions: {
     babelOptions: {
       presets: [require.resolve('next/babel')],
     },
   },
-};
+}
