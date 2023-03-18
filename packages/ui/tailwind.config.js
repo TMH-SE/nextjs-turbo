861137856a8544dffaedb2dd6fe33924477b54fa
TMH-SE <hieuhutieu98@gmail.com>
2023-03-18 10:58:03 +0700

    Config Tailwind in ui package

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,7 @@
+import sharedTailwindConfig from 'tailwind-config/tailwind.config'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...sharedTailwindConfig,
+  prefix: 'ui-',
+}
