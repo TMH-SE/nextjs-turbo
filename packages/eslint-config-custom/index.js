021346fd43ee6023806d41673cdf45a03e2c2b80
TMH-SE <hieuhutieu98@gmail.com>
2023-03-20 09:22:00 +0700

    Update eslint for new jsx transform

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['next/core-web-vitals', 'turbo', 'prettier', 'plugin:prettier/recommended'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
   parserOptions: {
     babelOptions: {
